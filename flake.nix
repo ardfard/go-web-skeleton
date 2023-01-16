58c9bd1e6e41f473bd465ac9aa674b377e4c3361
Ardin Fardiansyah <ardfarde@gmail.com>
2023-01-16 18:08:13 +0700

    init

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "A simple Go package";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+
+  # Flake utils to make it easier to work with flakes.
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      packageName = "go-web-skeleton";
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+      rec
+      {
+        packages = flake-utils.lib.flattenTree {
+          ${packageName} = pkgs.buildGoModule {
+            pname = "${packageName}";
+            inherit version;
+            # In 'nix develop', we don't need a copy of the source tree
+            # in the Nix store.
+            src = ./.;
+
+            # This hash locks the dependencies of this package. It is
+            # necessary because of how Go requires network access to resolve
+            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
+            # details. Normally one can build with a fake sha256 and rely on native Go
+            # mechanisms to tell you what the hash should be or determine what
+            # it should be "out-of-band" with other tooling (eg. gomod2nix).
+            # To begin with it is recommended to set this, but one must
+            # remeber to bump this hash when your dependencies change.
+            #vendorSha256 = pkgs.lib.fakeSha256;
+
+            vendorSha256 = "sha256-iZBgiAQG5/CGTCoxHrMuFQirRzG15s6zrf3rD3c3ny0=";
+          };
+        };
+        defaultPackage = packages.${packageName};
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go-tools
+            gopls
+            delve
+          ];
+          inputsFrom = [ defaultPackage ];
+        };
+      });
+
+  # The default package for 'nix build'. This makes sense if the
+  # flake provides only one package or there is a clear "main"
+  # package.
+
+}
