2abf0bfeecbaf21517a08b99a8150d466e2bfe8c
Ardin Fardiansyah <ardfarde@gmail.com>
2023-01-25 12:49:17 +0700

    add github action

--- flake.nix
+++ flake.nix
@@ -23,27 +23,34 @@
       in
       rec
       {
-        packages = flake-utils.lib.flattenTree {
-          ${packageName} = pkgs.buildGoModule {
-            pname = "${packageName}";
-            inherit version;
-            # In 'nix develop', we don't need a copy of the source tree
-            # in the Nix store.
-            src = ./.;
+        packages = flake-utils.lib.flattenTree
+          {
+            ${packageName} = pkgs.buildGoModule {
+              pname = "${packageName}";
+              inherit version;
+              # In 'nix develop', we don't need a copy of the source tree
+              # in the Nix store.
+              src = ./.;
 
-            # This hash locks the dependencies of this package. It is
-            # necessary because of how Go requires network access to resolve
-            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-            # details. Normally one can build with a fake sha256 and rely on native Go
-            # mechanisms to tell you what the hash should be or determine what
-            # it should be "out-of-band" with other tooling (eg. gomod2nix).
-            # To begin with it is recommended to set this, but one must
-            # remeber to bump this hash when your dependencies change.
-            #vendorSha256 = pkgs.lib.fakeSha256;
+              # This hash locks the dependencies of this package. It is
+              # necessary because of how Go requires network access to resolve
+              # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
+              # details. Normally one can build with a fake sha256 and rely on native Go
+              # mechanisms to tell you what the hash should be or determine what
+              # it should be "out-of-band" with other tooling (eg. gomod2nix).
+              # To begin with it is recommended to set this, but one must
+              # remeber to bump this hash when your dependencies change.
+              #vendorSha256 = pkgs.lib.fakeSha256;
 
-            vendorSha256 = "sha256-iZBgiAQG5/CGTCoxHrMuFQirRzG15s6zrf3rD3c3ny0=";
+              vendorSha256 = "sha256-MJrxperTBqZAScrbWW3cIZ+pwCQkykx6XsP52Jgb1cg=";
+            };
+            dockerImage = pkgs.dockerTools.buildImage {
+              name = "go-web-skeleton";
+              config = {
+                Cmd = [ "${packages.${packageName}}/bin/server" ];
+              };
+            };
           };
-        };
         defaultPackage = packages.${packageName};
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
